28325960c7196f6578fcb92ae468da5b584b9934
Gracious Mashasha <graccasoft263@gmail.com>
2023-03-29 09:27:00 +0200

    init

---
+++ src/main/java/com/graccasoft/paxful/PaxfulApplication.java
@@ -0,0 +1,13 @@
+package com.graccasoft.paxful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaxfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graccasoft/paxful/PaxfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graccasoft.paxful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaxfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
